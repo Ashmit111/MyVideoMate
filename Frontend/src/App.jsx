88a4c78576cf1cb24f0ec87b3bebc9d96726f9bc
Ashmit111 <ashmitsingh862@gmail.com>
2024-11-23 19:18:13 +0530

    ShadCN Installation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,48 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+// import { useState, useEffect } from 'react'
+// import './App.css'
+// import axios from 'axios'
 
-function App() {
-  const [healthCheck, sethealthCheck] = useState(0)
-  useEffect(() => {
-    axios.get('/api/v1/healthcheck')
-    .then((response)=>{
-      sethealthCheck(response.data)
-      console.log(response)
-      console.log(response.data);
+// function App() {
+//   const [healthCheck, sethealthCheck] = useState(0)
+//   useEffect(() => {
+//     axios.get('/api/v1/healthcheck')
+//     .then((response)=>{
+//       sethealthCheck(response.data)
+//       console.log(response)
+//       console.log(response.data);
       
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-  })
+//     })
+//     .catch((error)=>{
+//       console.log(error)
+//     })
+//   })
   
 
+//   return (
+//     <>
+//     <div>
+//     <h2>HealthCheck Report</h2>
+//     <ul>
+//       <li>{healthCheck.statusCode}</li>
+//       <li>{healthCheck?.data}</li>
+//       <li>{healthCheck.message}</li>
+//       <li>{healthCheck.success}</li>
+//     </ul>
+//     </div>
+//      </>
+//   )
+// }
+
+// export default App
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function Home() {
   return (
-    <>
-    <div>
-    <h2>HealthCheck Report</h2>
-    <ul>
-      <li>{healthCheck.statusCode}</li>
-      <li>{healthCheck?.data}</li>
-      <li>{healthCheck.message}</li>
-      <li>{healthCheck.success}</li>
-    </ul>
+    <div className="px-11 flex gap-7 bg-slate-500">
+      <Input type="email" placeholder="Email" />
+      <Button className="bg-destructive text-3xl font-extrabold" >Click me</Button>
     </div>
-     </>
   )
 }
 
-export default App
