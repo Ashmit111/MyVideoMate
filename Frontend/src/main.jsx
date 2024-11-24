23564621bd258fd96a468cbee9c10aab6f177e52
Ashmit111 <ashmitsingh862@gmail.com>
2024-11-24 21:56:56 +0530

    main jsx update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import App from './App'
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+
+//   )
+// )    
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
+  // <RouterProvider router={router} />
 )
