5b179b73239209e6ee1935dd59af9febb4cc9266
Ashmit111 <ashmitsingh862@gmail.com>
2024-11-25 22:21:36 +0530

    Update

--- Frontend/pages/Home.jsx
+++ Frontend/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { RiVideoAddLine, RiNotification3Line } from "react-icons/ri";
 import { FiMenu, FiSearch } from "react-icons/fi";
-import { BiLogOut } from "react-icons/bi";
-import { FaHistory, FaUserCircle } from "react-icons/fa";
+import { BiLogOut, BiSolidLike } from "react-icons/bi";
+import { FaHistory  } from "react-icons/fa";
 import { MdSubscriptions, MdVideoLibrary } from "react-icons/md";
+import { IoSettings } from "react-icons/io5";
+
 
 
 const Home = () => {
@@ -12,10 +14,13 @@ const Home = () => {
     const [searchQuery, setSearchQuery] = useState("");
 
     const sideItems = [
-        { icon: <FaRegCompass className="w-6 h-6" />, label: "Channel" },
+        { icon: <BiSolidLike className="w-6 h-6" />, label: "Liked Videos" },
+        { icon: <FaRegCompass className="w-6 h-6" />, label: "My Channel" },
         { icon: <MdSubscriptions className="w-6 h-6" />, label: "Subscriptions" },
         { icon: <MdVideoLibrary className="w-6 h-6" />, label: "My Playlists" },
-        { icon: <FaHistory className='w-6 h-6'/>, label: "Watch History" } 
+        { icon: <FaHistory className="w-6 h-6"/>, label: "Watch History" }, 
+        { icon: <IoSettings className="w-6 h-6"/>, label: "Settings"},
+        { icon: <BiLogOut className="w-6 h-6" />, label: "Log Out" }
       ];
   return (
     <div>
