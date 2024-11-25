aef81fff0035914cb87168a3f0769f441b88e1e7
Ashmit111 <ashmitsingh862@gmail.com>
2024-11-25 22:38:17 +0530

    Main.jsx Update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Home from 'pages/Home'
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+      <Route path="/Home" element={<Home/>}/>
+    </>
+  )
+);
+   
 
-//   )
-// )    
-
-createRoot(document.getElementById('root')).render(
-  <App />
-  // <RouterProvider router={router} />
+createRoot(document.getElementById('root')).render( 
+  <RouterProvider router={router} />
 )
