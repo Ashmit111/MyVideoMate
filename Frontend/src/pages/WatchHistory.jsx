541ab61c96dd12d9fa56fc8939de934adff1acec
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-03 22:48:06 +0530

    Minor Update

--- Frontend/src/pages/WatchHistory.jsx
+++ Frontend/src/pages/WatchHistory.jsx
@@ -159,6 +159,12 @@ function WatchHistory() {
   return (
     <div className='h-screen bg-black w-full overflow-y-scroll'>
       <nav className='w-full fixed bg-black h-16 flex items-center z-50 border-b border-gray-600'>
+        <div className='flex items-center py-3'>
+          <div className="flex gap-2 items-center px-3 ml-2">
+            <img src="./public/mytube.svg" alt="Logo" className='w-8 h-8 pt-1'/>
+            <h2 className='pt-1 text-lg text-white'>MyTube</h2>
+          </div>
+        </div>
          <div className='mx-auto'>
             <h2 className='text-white font-bold text-2xl pt-3'>Watch History</h2>
          </div>
