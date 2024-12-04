87f87356d94235db37028e171c5c54bdf2d8ad5c
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-04 21:49:08 +0530

    Update

--- Frontend/src/components/ui/playlistCard.jsx
+++ Frontend/src/components/ui/playlistCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PlaylistCard({ playlist }) {
     const image = 'https://imgs.search.brave.com/K0FWPeLcYsy2P7eV2k7dvd_3YcG7fNkr3OSCjeO4fqc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cHJvZC53ZWJzaXRl/LWZpbGVzLmNvbS82/MmQxMGJjZjlkYjNk/ZDI5ZTVmZGJiMmUv/NjYwYmJmMTU0NTU3/YTgwOGU5MjQ1MGI4/X3N1bW1lci1oaXRz/LXNwb3RpZnktcGxh/eWxpc3QtbWluLmpw/ZWc' 
     return (
-        <div className="max-w-sm h-64 rounded-lg shadow-2xl bg-black text-white overflow-hidden flex flex-col cursor-pointer">
+        <div className="max-w-sm h-64 rounded-lg shadow-2xl bg-black text-white overflow-hidden flex flex-col cursor-pointer hover:bg-slate-900">
           {/* Thumbnail */}
           <div className="relative h-40">
             <img
