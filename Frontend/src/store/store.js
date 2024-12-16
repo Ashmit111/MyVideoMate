3b753221b3ba08e382bd727c3bc6ae989c640ed7
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-16 11:29:25 +0530

    Store Config for Auth

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,7 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./Features/authSlice";   
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";   
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-    
-})
+  reducer: {
+    auth: persistedAuthReducer,  // Use the persisted auth reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
 
-export default store
+export const persistor = persistStore(store);
+
+export default store;
