5b50068047c2833ef2c1c9228b6b4748f845b2a4
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-16 22:54:17 +0530

    Provider and Persist Config

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store, {persistor} from './store/store'
+import { PersistGate } from 'redux-persist/integration/react' 
 import App from './App'
 import Home from './pages/Home.jsx'
 import SearchResultPage from './pages/SearchResultPage'
@@ -24,6 +27,10 @@ const router = createBrowserRouter(
 );
    
 
-createRoot(document.getElementById('root')).render( 
-  <RouterProvider router={router} />
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}> 
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider> 
 )
