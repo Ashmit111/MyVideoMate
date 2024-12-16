d15c458833050832e8c6ea4536276ab630725108
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-16 11:31:17 +0530

    Import update

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./Features/authSlice";   
+import authReducer from "../Features/authSlice" ;   
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";   
 
