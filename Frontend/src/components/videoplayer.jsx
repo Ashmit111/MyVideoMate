84df284a26fac1bbaa376f25ef9dff9d0f9a5e0f
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-20 15:03:53 +0530

    Fixed Poster

--- Frontend/src/components/videoplayer.jsx
+++ Frontend/src/components/videoplayer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const VideoPlayer = ({ videoUrl }) => {
+const VideoPlayer = ({ video }) => {
     return (
       <div className="w-full aspect-video mb-4">
         <video
           className="w-full h-full rounded-lg"
           controls
-          src={videoUrl || "https://videos.pexels.com/video-files/27857022/12245086_2560_1440_60fps.mp4"}
-          poster="https://via.placeholder.com/800x450"
+          src={video.url || "https://videos.pexels.com/video-files/27857022/12245086_2560_1440_60fps.mp4"}
+        poster={video.thumbnail}
         ></video>
       </div>
     );
