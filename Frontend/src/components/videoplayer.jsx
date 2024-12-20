c450fad8006d61c143628bcfb6635e056e0d3fdb
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-20 21:29:03 +0530

    Fixed Issue

--- Frontend/src/components/videoplayer.jsx
+++ Frontend/src/components/videoplayer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const VideoPlayer = ({ video }) => {
+  const { videoFile, thumbnail } = video;
     return (
       <div className="w-full aspect-video mb-4">
         <video
           className="w-full h-full rounded-lg"
           controls
-          src={video.url || "https://videos.pexels.com/video-files/27857022/12245086_2560_1440_60fps.mp4"}
-        poster={video.thumbnail}
+          src={videoFile || "https://videos.pexels.com/video-files/27857022/12245086_2560_1440_60fps.mp4"}
+        poster={thumbnail}
         ></video>
       </div>
     );
