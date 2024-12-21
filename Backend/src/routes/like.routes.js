559bda04e9412f446b16543fec376bb26d9112dd
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-21 21:57:54 +0530

    Added one more route

--- Backend/src/routes/like.routes.js
+++ Backend/src/routes/like.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
     getLikedVideos,
     toggleCommentLike,
-    toggleVideoLike,
+    toggleAndGetLikes,
     toggleTweetLike,
 } from "../controllers/like.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
@@ -10,7 +10,9 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/v/:videoId")
+.get(toggleAndGetLikes)
+.post(toggleAndGetLikes);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
 router.route("/videos").get(getLikedVideos);
