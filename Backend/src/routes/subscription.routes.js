a05814dcfb36be99af9cf500e470e732b0ac0976
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-21 22:52:59 +0530

    Fixed Route

--- Backend/src/routes/subscription.routes.js
+++ Backend/src/routes/subscription.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
     getSubscribedChannels,
     getUserChannelSubscribers,
-    toggleSubscription,
+    toggleAndGetSubscription,
 } from "../controllers/subscription.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -11,9 +11,10 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
+    .get(toggleAndGetSubscription)
+    .post(toggleAndGetSubscription);
 
+router.route("/cs/:channelId").get(getSubscribedChannels)   
 router.route("/u/:subscriberId").get(getUserChannelSubscribers);
 
 export default router
