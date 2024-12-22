7adf1929ee1ff062e996d76ed7f0806863ee9e00
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-22 21:10:39 +0530

    Update

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -154,7 +154,8 @@ const loginUser = asyncHandler(async (req, res) =>{
         httpOnly: true,
         secure: true
     }
-
+    console.log(loggedInUser);
+    
     return res
     .status(200)
     .cookie("accessToken", accessToken, options)
