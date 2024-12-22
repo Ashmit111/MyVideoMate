ce3f2b2fcd102863d6fc3f3ba45d9dc7eb99b27c
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-22 21:11:54 +0530

    Update

--- Frontend/src/Features/authSlice.js
+++ Frontend/src/Features/authSlice.js
@@ -1,27 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status: false,
-    userData: null
-}
+  status: false, // Represents whether the user is logged in
+  userData: null, // Holds user details when logged in
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers:{
-        login: (state, action) => {
-            state.status = true,
-            state.userData = action.payload
-        },
-        logout: (state) => {
-            state.status = false,
-            state.userData = null
-        },
-        updateUser(state, action) {
-            state.userData = { ...state.user, ...action.payload };  
-        },
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = { 
+        username: action.payload.username, 
+        fullName: action.payload.fullName,
+        email: action.payload.email, 
+        avatar: action.payload.avatarUrl, 
+        coverImage: action.payload.coverImageUrl, 
+        accessToken: action.payload.accessToken 
+      };
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null; // Clear user data on logout
+    },
+    updateUser: (state, action) => {
+      if (state.userData) {
+        state.userData = { ...state.userData, ...action.payload };
+      }
+    },
+  },
+});
 
-export const {login, logout, updateUser} = authSlice.actions;
+export const { login, logout, updateUser } = authSlice.actions;
 export default authSlice.reducer;
