aef8eec9e0cc2ed91a7bf797b38beb5f15e9c53b
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-24 19:26:53 +0530

    Add route for retrieving channel views with JWT verification

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -10,7 +10,8 @@ import {
     updateUserCoverImage, 
     getUserChannelProfile, 
     getWatchHistory, 
-    updateAccountDetails
+    updateAccountDetails,
+    getChannelViews
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -35,6 +36,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
+router.route("/channel-views").get(verifyJWT, getChannelViews)
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
