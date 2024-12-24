a7c129a37f8af28c710b14bd0f298c09b43cb0be
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-24 20:21:02 +0530

    Add Like model import in user controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Like } from "../models/like.model.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 import fs from 'fs'
