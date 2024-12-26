6c4d969477e23fb7d7616a7e046a6d03fea23bbe
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-26 22:29:11 +0530

    Update AudienceCard component to display total videos and improve layout

--- Frontend/src/components/ui/AudienceCard.jsx
+++ Frontend/src/components/ui/AudienceCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
  
-const AudienceCard = ({ user }) => (
+const AudienceCard = ({ user, totalVideos }) => {
+
+  console.log(user);
+   
+  return (
+  
     <div className="flex justify-between items-center p-4 border border-gray-300 rounded-md shadow-sm">
       {/* Left side: Avatar and Username */}
       <div className="flex items-center space-x-4">
@@ -9,13 +14,13 @@ const AudienceCard = ({ user }) => (
           alt="User Avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
-        <span className="font-medium text-lg">{user.username}</span>
+        <span className="font-medium text-lg text-white">{user.username}</span>
       </div>
   
       {/* Right side: Total Videos */}
       <div>
-        <span className="text-gray-600">Total Videos: {user.totalVideos}</span>
+        <span className="text-gray-600">Total Videos: {totalVideos}</span>
       </div>
     </div>
-  ); 
+  )}; 
 export default AudienceCard
