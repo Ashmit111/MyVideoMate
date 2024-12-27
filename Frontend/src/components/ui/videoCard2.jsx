063db94e49fbb00e402c21ec75fe684deb20e753
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-27 18:17:30 +0530

    Refactor VideoCard2 layout and styles for improved presentation

--- Frontend/src/components/ui/videoCard2.jsx
+++ Frontend/src/components/ui/videoCard2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoCard2 = ({ video }) => {
     return (
-      <div className="w-full h-36 rounded-lg shadow-lg bg-black text-white overflow-hidden cursor-pointer flex hover:bg-[#1e1e1e]">
+      <div className="w-[395px] h-36 rounded-lg shadow-lg bg-black text-white overflow-hidden cursor-pointer flex hover:bg-[#1e1e1e]">
         {/* Thumbnail */}
         <div className="relative h-40">
           <img
@@ -10,17 +10,18 @@ const VideoCard2 = ({ video }) => {
             alt={video.title}
             className="w-auto h-36 object-cover"
           />
-          <span className="absolute bottom-2 right-2 bg-black bg-opacity-75 text-xs px-2 pb-2  rounded-md">
+          <span className="absolute bottom-3 right-2 bg-black bg-opacity-75 text-xs px-2 py-1 rounded-md">
             {video.formattedDuration}
           </span>
         </div>
   
         {/* Video Info */}
-            <div className="flex-1 flex-col space-y-3 py-4 px-3">
+            <div className="flex-1 flex-col space-y-4 py-4 px-3">
                 <div>
                     <h3 className="text-sm font-semibold line-clamp-2">
                     {video.title}
                     </h3>
+                    <p className='text-[10px]'>{video.description}</p>
                 </div>
                 <div>
                     <p className="text-xs text-gray-400">
@@ -28,11 +29,11 @@ const VideoCard2 = ({ video }) => {
                     {/* {video.timeAgo} */}
                     </p>
                 </div>
-                <div className='flex gap-4'>
+                <div className='flex gap-2'>
                     <img
                     src={video.owner.avatar}
                     alt={video.owner.username}
-                    className="w-10 h-10 rounded-full" />
+                    className="w-9 h-9 rounded-full" />
                     <p className="text-xs text-gray-400 mt-3">
                     {video.owner.username}
                     </p>
