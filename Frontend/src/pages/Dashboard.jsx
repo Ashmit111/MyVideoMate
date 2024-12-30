7e05b3a03bd85957d53f614c3bf300d45bcc088c
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-30 22:57:51 +0530

    Integrate Navbar component into Dashboard for improved layout consistency

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { MdDelete } from "react-icons/md";
 import axiosInstance from '@/utils/axiosInstance'; 
 import { useForm } from 'react-hook-form';
 import {toast} from 'react-hot-toast';
+import Navbar from '@/components/ui/Navbar';
 
 function Dashboard() {
   const [videos, setVideos] = useState([]);
@@ -113,19 +114,7 @@ function Dashboard() {
   return (
     <div className="h-screen bg-black w-screen overflow-y-scroll">
       {/* Navbar */}
-      <nav className="w-full fixed bg-black h-16 flex items-center z-50 border-b border-gray-600">
-        <div className="flex items-center py-3">
-          <div className="flex gap-2 items-center px-3 ml-2">
-            <img src="./public/mytube.svg" alt="Logo" className="w-8 h-8 pt-1" />
-            <h2 className="pt-1 text-lg text-white">MyTube</h2>
-          </div>
-        </div>
-        <div className="mx-auto">
-          <h2 className="text-white font-bold text-2xl pt-3 flex gap-2">
-            Admin Dashboard
-          </h2>
-        </div>
-      </nav>
+      <Navbar/> 
 
       {/* Content Section */}
       <div className="pt-16 bg-black text-white w-full">
