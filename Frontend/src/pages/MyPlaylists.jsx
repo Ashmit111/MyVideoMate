7a87e41f742cba767a7071f5a5550213a4493562
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-30 23:31:00 +0530

    Refactor sidebar implementation in multiple pages for consistency

--- Frontend/src/pages/MyPlaylists.jsx
+++ Frontend/src/pages/MyPlaylists.jsx
@@ -10,23 +10,14 @@ import CreatePlaylistModal from '@/components/ui/CreatePlaylistModal';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '@/components/ui/Navbar';
+import Sidebar from '@/components/ui/Sidebar';
 
 function MyPlaylists() {
     const [isLoading, setIsLoading] = useState(false);  
     const [playlists, setPlaylists] = useState([]);
     const [modal, setModal] = useState(false);  
-    const editModal = false;
-    const navigate = useNavigate();
- 
-    const sideItems = [
-      { icon: <BiLike className="w-6 h-6" />, label: "Liked Videos", path: '/likedVideos' },
-      { icon: <FaRegCompass className="w-6 h-6" />, label: "My Channel", path: '/dashboard' },
-      { icon: <MdSubscriptions className="w-6 h-6" />, label: "Audience", path: '/audience' },
-      { icon: <MdVideoLibrary className="w-6 h-6" />, label: "My Playlists", path: '/myplaylist' },
-      { icon: <FaHistory className="w-6 h-6"/>, label: "Watch History", path: '/watchHistory' }, 
-      { icon: <IoSettings className="w-6 h-6"/>, label: "Settings", path: '/dashboard'} 
-    ];
-
+    const editModal = false; 
+    
     const fetchPlaylist = async () =>{
         const response = await axiosInstance.get('/playlist');
         console.log(response.data);
@@ -45,28 +36,7 @@ function MyPlaylists() {
       //sidebar 
         {/* Sidebar */}
         <div className="fixed top-14 left-0 h-[calc(100vh-3.5rem)] w-60 bg-black flex flex-col gap-3 overflow-y-auto pt-4 border-r border-gray-600">
-          <div>
-            {sideItems.map((item, index) => (
-              <button
-                key={index}
-                className="w-full flex cursor-pointer items-center px-8 py-3 text-white bg-transparent outline-none border-none focus:outline-none focus:bg-[#1e1e1e] focus:ring-0 hover:bg-[#1e1e1e]"
-                aria-label={item.label}
-                onClick={() => navigate(item.path)}
-              >
-                {React.cloneElement(item.icon, { className: 'h-5 w-5 text-white' })}
-                <span className="ml-4 text-sm">{item.label}</span>
-              </button>
-            ))}
-          </div>
-          <div className="mt-auto mb-2">
-            <button
-              className="w-full flex items-center cursor-pointer px-8 py-3 text-white bg-transparent outline-none border-none focus:outline-none focus:ring-0 hover:bg-[#1e1e1e] focus:bg-[#1e1e1e]"
-              aria-label="Log Out"
-            >
-              <BiLogOut className="h-5 w-5 text-red" />
-              <span className="ml-4 text-sm">Log Out</span>
-            </button>
-          </div>
+          <Sidebar/> 
         </div>
  
       {/* Main Content */}
