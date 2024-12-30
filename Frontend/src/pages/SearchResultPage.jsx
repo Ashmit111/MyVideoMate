2550f2a92a57cbf2e7b0e3d20c64b7f522d558b9
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-30 22:58:25 +0530

    Update

--- Frontend/src/pages/SearchResultPage.jsx
+++ Frontend/src/pages/SearchResultPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useState, useEffect } from 'react';  
-import { FiSearch } from "react-icons/fi";
 import { BiLogOut, BiLike } from "react-icons/bi";
-import { FaHistory, FaRegCompass, FaRegUser  } from "react-icons/fa";
+import { FaHistory, FaRegCompass } from "react-icons/fa";
 import { MdSubscriptions, MdVideoLibrary } from "react-icons/md";
 import { IoSettings } from "react-icons/io5"; 
-import VideoCard3 from '@/components/ui/videoCard3';
-import axios from 'axios'; 
+import VideoCard3 from '@/components/ui/videoCard3'; 
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axiosInstance from '@/utils/axiosInstance';
+import Navbar from '@/components/ui/Navbar';
 
 function SearchResultPage() { 
   const [loading, setLoading] = useState(true); 
@@ -64,18 +63,7 @@ function SearchResultPage() {
         const remainingSeconds = seconds % 60;
         return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
      } 
-    }, [query]);
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (searchQuery.trim()) {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-            navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
-        }, 0); // Simulates a delay, adjust as needed
-    }
-};
+    }, [query]); 
  
     // if (loading) {
     //   return (
@@ -92,56 +80,7 @@ function SearchResultPage() {
 
 return (
   <div className='h-screen bg-black w-full overflow-y-scroll'>
-    <nav className='w-full fixed bg-black h-16 flex items-center z-50 border-b border-slate-500'>
-      <div className='flex items-center py-3'>
-        <div className="flex gap-2 items-center px-3 ml-2">
-          <img src="./public/mytube.svg" alt="Logo" className='w-8 h-8 pt-1'/>
-          <h2 className='pt-1 text-lg text-white'>MyTube</h2>
-        </div>
-      </div>
-      <div className="flex-1 max-w-xl mx-auto mt-2 items-center justify-center">
-        <div className="flex items-center space-x-2">
-          <form onSubmit={handleSearch} className="relative flex items-center flex-grow -mt-1">
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="w-full px-4 py-[6px] rounded-2xl bg-black border-[1.7px] border-white text-white placeholder-white"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              type="submit"
-              className="px-6 py-2 bg-transparent rounded-full outline-none border-none focus:outline-none focus:ring-0 hover:bg-[#313030] focus:bg-[#313030]"
-              aria-label="Submit search"
-              // onClick={handleSearch}
-            >
-            {/* {loading ? (
-              <div className="w-5 h-5 border-2 border-gray-500 border-t-red-500 rounded-full animate-spin"></div>
-              ) : (
-              <FiSearch className="h-5 w-5 text-white" />
-              )} */}
-            </button>
-          </form> 
-        </div>
-      </div>
-      <div className="items-center pr-8">
-        {profilePic.length>0 ? (
-          <img
-            src={profilePic} // Fetched from DB
-            alt="Profile"
-            className="w-10 h-10 rounded-full object-cover border-2 border-white"
-            aria-label="User profile"
-            />
-            ) : (
-            <FaRegUser
-              className="w-5 h-5 text-white"
-              aria-label="Default user icon"
-            />
-        )}
-      </div>
- 
-    </nav>
+    <Navbar/> 
     //sidebar 
       {/* Sidebar */}
       <div className="fixed top-14 left-0 h-[calc(100vh-3.5rem)] w-60 bg-black flex flex-col gap-3 overflow-y-auto pt-4 border-r border-slate-500">
