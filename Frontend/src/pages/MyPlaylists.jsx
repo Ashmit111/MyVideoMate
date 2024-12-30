17a142b4e37fcc6261c92b5576d54312d4156638
Ashmit111 <ashmitsingh862@gmail.com>
2024-12-30 22:58:09 +0530

    Integrate Navbar component into MyPlaylists page for improved layout consistency

--- Frontend/src/pages/MyPlaylists.jsx
+++ Frontend/src/pages/MyPlaylists.jsx
@@ -9,6 +9,7 @@ import axiosInstance from '@/utils/axiosInstance';
 import CreatePlaylistModal from '@/components/ui/CreatePlaylistModal';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '@/components/ui/Navbar';
 
 function MyPlaylists() {
     const [isLoading, setIsLoading] = useState(false);  
@@ -40,17 +41,7 @@ function MyPlaylists() {
     };
   return (
     <div className='h-screen bg-black w-full overflow-y-scroll'>
-      <nav className='w-full fixed bg-black h-16 flex items-center z-50 border-b border-gray-600'>
-        <div className='flex items-center py-3'>
-          <div className="flex gap-2 items-center px-3 ml-2">
-            <img src="./public/mytube.svg" alt="Logo" className='w-8 h-8 pt-1'/>
-            <h2 className='pt-1 text-lg text-white'>MyTube</h2>
-          </div>
-        </div>
-         <div className='mx-auto'>
-            <h2 className='text-white font-bold text-2xl pt-3'>My Playlists</h2>
-         </div>
-      </nav>
+      <Navbar/>
       //sidebar 
         {/* Sidebar */}
         <div className="fixed top-14 left-0 h-[calc(100vh-3.5rem)] w-60 bg-black flex flex-col gap-3 overflow-y-auto pt-4 border-r border-gray-600">
